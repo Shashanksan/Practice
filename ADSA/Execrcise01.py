3f4abb526185b149591fa735ddd6df7635989ca0
Shashanksan <shashanksiddesh1234@gmAIL.COM>
2026-05-31 02:57:55 +0530

    added

--- ADSA/Execrcise01.py
+++ ADSA/Execrcise01.py
@@ -1,45 +1,35 @@
 from array import array
-
 arr=list(map(int,input().split()))
-
-    
-
 def insert():
-      pos=int(input())
-      val=int(input())
-      print("lenght",len(arr))
-      if pos>len(arr):
-          print("Invalid position")
-      else:
+    pos=int(input())
+    val=int(input())
+    if pos>len(arr):
+        print("Invalid position.")
+    else :
         arr.insert(pos,val)
-        print("Inserted",val,"at position", pos)
-     
+        print(f"Inserted {val} at position {pos}.")
 def delete():
-  pos=int(input())
-  if (pos>=0) and (pos<len(arr)):
-  
-      arr.pop(pos)
-      print("Deleted element","from position" ,pos)
-  else:
-      print("Invalid position.")
-
+    pos=int(input())
+    if(pos>=0)and(pos<len(arr)):
+        print(f"Deleted element {arr[pos]} from position {pos}.")
+        arr.pop(pos)
+    else:
+        print("Invalid position.")
+      
+        
+    
 def view():
     if(arr.count==0):
-        print("Array is empty.")
+        print("Array is empty")
     else:
-        
-        print("Current array:",arr)
+        print("Current array:",*arr)
 while(True):
-      choice=int(input())
-     
-      match choice:
-          case 1:
-              insert()
-          case 2:
-               delete()
-          case 3:
-                view()
-          case 4:
-              exit() 
-        
-    
+    choice=int(input())
+    if choice==1:
+     insert()
+    elif choice==2:
+     delete()
+    elif choice==3:
+     view()
+    elif choice==4:   
+     exit()
